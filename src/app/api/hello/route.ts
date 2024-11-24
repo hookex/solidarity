ec9adbe289c6ff3b4090a7d44a18e161436c0aa4
hookex <cookmycode@gmail.com>
2024-11-24 11:55:03 +0800

    ...

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,6 +1,5 @@
-import { IncomingMessage } from 'http';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(request: IncomingMessage) {
+export async function GET(request: NextRequest) {
     return NextResponse.json({ message: 'Hello, Hooke!' });
 }
