b0b82ee8f783d8e73e86bafffc166bf550b2a331
hookex <cookmycode@gmail.com>
2024-11-24 23:18:23 +0800

    ...

--- src/app/components/full-screen-layout/index.tsx
+++ src/app/components/full-screen-layout/index.tsx
@@ -1,6 +1,11 @@
 // components/FullScreenLayout.js
 import styles from './index.module.css';
+import {ReactNode} from "react";
 
-export default function FullScreenLayout({ children }) {
+interface FullScreenLayoutProps {
+    children: ReactNode;
+}
+
+export default function FullScreenLayout({ children }: FullScreenLayoutProps) {
     return <div className={styles.container}>{children}</div>;
 }
